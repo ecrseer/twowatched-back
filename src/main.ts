85c98ad7e18da9596612a6f2ae9343d42ce751ae
ecrseer <juustino@live.com>
2023-11-08 23:18:15 -0300

    install mongoose

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Uncomment these lines to use the Redis adapter:
+  // const redisIoAdapter = new RedisIoAdapter(app);
+  // await redisIoAdapter.connectToRedis();
+  // app.useWebSocketAdapter(redisIoAdapter);
+  app.enableCors();
   await app.listen(3030);
 }
 bootstrap();
---
+++ src/twochat.gateway.ts
@@ -0,0 +1,42 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({ cors: true })
+export class TwochatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  afterInit(server: any) {
+    console.log('🚀 ~ afterInit ~ server:');
+  }
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: any, ...args: any[]) {
+    console.log('connected');
+  }
+  handleDisconnect(client: any) {
+    console.log('disconnected');
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(
+    @MessageBody() data: { abc: number },
+    @ConnectedSocket() client: Socket,
+  ) {
+    console.log('🚀 ~ data:', data);
+    const transformed = +data.abc * 2;
+    client.send({ transformed });
+    console.log('🚀 ~ transformed:', transformed);
+    // Handle received message
+    // this.server.emit('message', { transformed }); // Broadcast the message to all connected clients
+  }
+}
