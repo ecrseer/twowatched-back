0d8e9ecf12679553389698dc187e63f13fff7b5b
ecrseer <juustino@live.com>
2023-11-11 23:06:03 -0300

    fly 4

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   // await redisIoAdapter.connectToRedis();
   // app.useWebSocketAdapter(redisIoAdapter);
   app.enableCors();
-  await app.listen(3030);
+  await app.listen(3000);
 }
 bootstrap();
