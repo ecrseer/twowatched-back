7d92657dc3565ddcf5c77544e5d1f9c6e0808024
Gabriel Justino Marques Couto <gabriel.justino@dsgtechnology.com.br>
2024-02-25 23:46:12 -0300

    newport

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   // await redisIoAdapter.connectToRedis();
   // app.useWebSocketAdapter(redisIoAdapter);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(3042);
 }
 bootstrap();
